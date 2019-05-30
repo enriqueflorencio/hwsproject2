212d63f94b0707100e0b62509bf8113f820ab71a
Enrique Florencio <enriqueflorencio@enriques-macbook-pro.local>
2019-05-29 19:37:42 -0700

    testing

--- hwsproject2/hwsproject2/ViewController.swift
+++ hwsproject2/hwsproject2/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Some code to check if it worked from this new laptop
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
